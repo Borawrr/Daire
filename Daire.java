f76a78cf893c65144d29c466171953fd5724bccf
Bora Tekin <pikacu1903@gmail.com>
2023-12-09 14:15:49 +0300

    Create Daire.java

---
+++ Daire.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Daire {
+    public static void main(String[] args) {
+        
+        Scanner scanner = new Scanner(System.in);
+
+        double π = 3.14;
+        int r;
+        int a;
+
+        System.out.println("Dairenizin Yarıçapını Giriniz : ");
+        r = scanner.nextInt();
+
+        System.out.println("Merkez Açısının Ölçüsünü Giriniz : ");
+        a = scanner.nextInt();
+
+        double DaireDilimiAlan = (π * (r*r) * a)/360;
+
+        System.out.println("Daire Diliminizin Alanı : " + DaireDilimiAlan);
+    }
+}
